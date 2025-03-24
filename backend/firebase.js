778c155d0255d5f7ad71a933cf3f86f2f9d6964d
Junjie Wu <z5347759@ad.unsw.edu.au>
2025-03-24 15:41:09 +1100

    initialised-firebase

---
+++ backend/firebase.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const admin = require("firebase-admin");
+
+const serviceAccount = {
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Fixes newline issue
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+module.exports = db;
